1cc98f648002fa43d4b5423c9301929cb532e4c5
omeraladen <killerpop1738@gmail.com>
2022-01-30 03:59:51 -0800

    adding a control to auth changes on firebase

--- src/App.js
+++ src/App.js
@@ -5,30 +5,57 @@ import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import SinInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 
 
-function App(){
-  return (
-    <div> 
-    
+class App extends React.Component{
+  constructor(){
+    super();
+
+    this.state ={
+      currentUser: null
+    }
+  }
+
+  // handle any Auth changes On firebase
+  componentDidMount(){
+    this.unsubscriberFromAuth = auth.onAuthStateChanged(user => {
+
+      this.setState({ currentUser: user });
+
+      console.log(user)
+    });
+  }
+
+  componentWillUnmount(){ // this mathod Close the Subscribion 
+    this.unsubscriberFromAuth();
+  }
+  
+
+  
+  render(){
+    return (
+      <div> 
       
-      <BrowserRouter>
-      <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage}/> 
-          <Route  path='/shop' component={ShopPage}/>
-          <Route  path='/signin' component={SinInAndSignUpPage}/>
-       </Switch>
-      </BrowserRouter>
-    
-
-    </div>
-  );
+        
+        <BrowserRouter>
+        <Header />
+          <Switch>
+            <Route exact path='/' component={HomePage}/> 
+            <Route  path='/shop' component={ShopPage}/>
+            <Route  path='/signin' component={SinInAndSignUpPage}/>
+         </Switch>
+        </BrowserRouter>
+      
+  
+      </div>
+    );
+  }
 }
 export default App;
 
 // To Fix the ERROR => Error: 'node-sass' version 5.0.0 is incompatible with ^4.0.0
-// Solution => isnpm install -g sass
+// Solution => is npm install -g sass
 //               npm install --save-dev sass 
 // ********hello this is new commit is added right now
