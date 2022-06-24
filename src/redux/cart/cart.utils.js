277e2cbf0e6fc35243cab74ae438574f4f98a3af
omeraladen <killerpop1738@gmail.com>
2022-06-24 10:53:37 +0200

    create a cart item component and connected to cart dropdown

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,18 +1,3 @@
-// const addItemToCart = (cartItems, cartItemToAdd) =>{
-//     const existingCartItem = cartItems.find(
-//         cartItem => cartItem.id == cartItemToAdd.id
-//     );
-
-//     if(existingCartItem) {
-//         return cartItems.map( cartItem => 
-//             cartItem.id == cartItemToAdd 
-//             ? {...cartItem, quantity: cartItem.quantity + 1}
-//             : cartItem
-//         )
-//     }
-
-//     return [...cartItems, {...cartItemToAdd, quantity: 1}]
-// };
 
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
